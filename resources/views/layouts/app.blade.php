c8885d3f008e2e6a8d1fe3804985addb8d2e0966
Patryk Mikołajczak <krawlof@gmail.com>
2026-01-22 15:30:52 +0100

    nav to houses and tournament rank

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,12 @@
                     <li><a href="#" class="nav-link px-2 text-white"><img src="/images/ustawienia.png" width="150px;"/></a></li>
                 </ul>
                 @endauth
+                @guest
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="{{ route('public.houses') }}" class="nav-link px-2 text-white"><img src="/images/punkty.png" width="150px;"/></a></li>
+                    <li><a href="{{ route('public.tournament') }}" class="nav-link px-2 text-white"><img src="/images/turniej.png" width="150px;"/></a></li>
+                </ul>
+                @endguest
                 <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
                     <input type="search"
                     class="form-control form-control-dark text-bg-dark"
